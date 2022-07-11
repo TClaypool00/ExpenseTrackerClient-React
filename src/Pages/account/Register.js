84552332317c3b07c71895d54bae1a1c2240189d
claypooltyler00 <claypooltyler00@gmail.com>
2022-07-10 21:42:14 -0500

    Added a login side

--- src/Pages/account/Register.js
+++ src/Pages/account/Register.js
@@ -1,9 +1,19 @@
 const Register = () => {
+    function hideShowForm() {
+        const loginForm = document.getElementById('loginForm');
+
+        if (loginForm.style.display === 'none') {
+            loginForm.style.display = 'block';
+        } else {
+            loginForm.style.display = 'none';
+        }
+    }
+
     return (
         <>
-            <h2>Reigster</h2>
+            <form method="post" id="registerForm">
+                <h2>Reigster</h2>
 
-            <form method="post">
                 <input type="text" id="txtFirstName" placeholder="First name" />
                 <input type="text" id="txtLastName" placeholder="Last name" />
                 <input type="email" id="email" placeholder="Email address" />
@@ -12,7 +22,21 @@ const Register = () => {
                 <input type="password" id="confirmPassword" placeholder="Confirm Password" />
 
                 <button type="submit" className="btn" id="btnRegister">Register</button>
+            </form>                        
+
+            <button className="btn" id="signWithEmail" onClick={hideShowForm}>Sign in with email</button>
+
+            <form method="post" id="loginForm">
+                <h2>Login</h2>
+
+                <input type="text" id="txtLoginEmail" placeholder="Email address" />
+                <input type="password" id="txtLoginPassword" placeholder="Password" />
+                <button type="submit" className="btn" id="btnLogin">Login</button>
             </form>
+
+            <button className="btn twitter-blue">Sign in with Google</button>
+            <button className="btn facebook-blue">Sign in with Facebook</button>
+            <button className="btn google-yellow">Sign in with Twitter</button>
         </>
     );
 }
